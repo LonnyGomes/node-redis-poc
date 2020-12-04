19a1ce12956a8b02a9d670f7d78b2d29d6ffe747
Lonny Gomes <code@lonnygomes.com>
2020-12-04 01:28:40 -0500

    feat: add hash example

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const { promisify } = require('util');
+const _ = require('lodash');
 const redis = require('redis');
 const connectionOps = {
     host: '127.0.0.1',
@@ -16,7 +17,7 @@ const connect = (options) => {
 
         client.on('connect', () => {
             console.log('connected');
-            const commands = ['get', 'set', 'end'];
+            const commands = ['get', 'set', 'end', 'hmset', 'hgetall'];
 
             const api = commands.reduce((finalObj, cmd) => {
                 finalObj[cmd] = promisify(client[cmd]).bind(client);
@@ -31,9 +32,20 @@ const connect = (options) => {
 const init = async () => {
     try {
         const client = await connect(connectionOps);
+
+        // key/values
         await client.set('docker', 'testing123');
         const result = await client.get('docker');
-        console.log('yes', result);
+        console.log('key/value result:', result);
+
+        // hashes
+        const obj = { a: 1, b: 2, c: 3, d: 4, hello: 'world' };
+        // convert the object to key/value pairs
+        const objPairs = _.chain(obj).toPairs().flatten().value();
+
+        await client.hmset('hash', objPairs);
+        const hashResult = await client.hgetall('hash');
+        console.log('hash:', hashResult);
 
         client.end(true);
     } catch (error) {
